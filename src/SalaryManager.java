c8e79248658bddedf3e79983b79ce50657acce63
ziyi0227 <1206881089@qq.com>
2023-04-23 11:08:36 +0800

    jdbc的init

--- src/SalaryManager.java
+++ src/SalaryManager.java
@@ -1,11 +1,22 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import javax.swing.*;
 
 public class SalaryManager {
-        public static void main(String[] args) {
-       	    LoginFrame lf=new LoginFrame();
-		    lf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    	    		    
+	public static void main(String[] args) throws Exception {
+		LoginFrame lf=new LoginFrame();
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		String url = "jdbc:mysql://123.60.142.8:3306/hov";
+		String username = "hov";
+		String password = "NX6f8baShtXnFEKj";
+		Connection conn = DriverManager.getConnection(url,username,password);
+
+		lf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		conn.close();
     }
 }
 
