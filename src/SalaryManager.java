8315327bc1dda2d6fe687a77cd3182fa2791e1e6
HovChen <510478741@qq.com>
2023-05-01 01:05:05 +0800

    修改LoginFrame和SignupFrame的UI

--- src/SalaryManager.java
+++ src/SalaryManager.java
@@ -5,18 +5,18 @@ import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 
 public class SalaryManager {
-	public static void main(String[] args) throws Exception {
-		LoginFrame lf=new LoginFrame();
+    public static void main(String[] args) throws Exception {
+        LoginFrame lf = new LoginFrame();
 
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		String url = "jdbc:mysql://123.60.142.8:3306/hov";
-		String username = "hov";
-		String password = "NX6f8baShtXnFEKj";
-		Connection conn = DriverManager.getConnection(url,username,password);
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String url = "jdbc:mysql://123.60.142.8:3306/hov";
+        String username = "hov";
+        String password = "NX6f8baShtXnFEKj";
+        Connection conn = DriverManager.getConnection(url, username, password);
 
-		lf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        lf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		conn.close();
+        conn.close();
     }
 }
 
